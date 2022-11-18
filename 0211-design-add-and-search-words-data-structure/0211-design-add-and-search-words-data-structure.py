ad5340efc0cb3cdcd2aa745962ec8b911217b7f6
Abraham Diress <85117882+abraham-diress@users.noreply.github.com>
2022-11-18 21:12:44 +0200

    Time: 14144 ms (29.06%), Space: 91.4 MB (5.11%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,42 +1,53 @@
 class WordDictionary:
 
     def __init__(self):
-        self.root = TrieNode() 
+        self.root = TrieNode()
         self.max_length = 0
+        
+
     def addWord(self, word: str) -> None:
-        current = self.root 
+        current = self.root
         
         for ch in word:
             idx = ord(ch) - ord('a')
             if not current.children[idx]:
                 current.children[idx] = TrieNode()
             current = current.children[idx]
-        
-        current.isEnd = True
+            
         self.max_length = max(self.max_length, len(word))
-
+        current.isEnd = True
+            
     def search(self, word: str) -> bool:
         if len(word) > self.max_length:
-                return False
+            return False
         
-        def modifiedSearch(i, node):
+        def dfs(i, node):
             if not node:
-                return False 
+                return False
+            
             if i == len(word):
                 return node.isEnd
             
-            if word[i] == '.':
+            if word[i] == ".":
                 for child in node.children:
-                    if child and modifiedSearch(i + 1, child):
+                    if child and dfs(i + 1, child):
                         return True
             else:
-                idx = ord(word[i]) - ord('a') 
-                if node.children[idx] and modifiedSearch(i + 1, node.children[idx]):
-                    return True
-                
-        return modifiedSearch(0, self.root)
-                        
+                idx = ord(word[i]) - ord('a')
+                return node.children[idx] and dfs(i + 1, node.children[idx])
+            
+            return False
+        
+        return dfs(0, self.root)
+
 class TrieNode:
     def __init__(self):
         self.children = [None for _ in range(26)]
-        self.isEnd = False
+        self.isEnd = False 
+        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
