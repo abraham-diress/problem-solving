29f9775d757f32f083263cb692919a41963388ac
Abraham Diress <85117882+abraham-diress@users.noreply.github.com>
2022-11-26 17:43:17 +0200

    Time: 2366 ms (19.33%), Space: 29.4 MB (41.09%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.py
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.py
@@ -1,27 +1,18 @@
 class Solution:
     def longestNiceSubarray(self, nums: List[int]) -> int:
-        count = [ 0 for _ in range(32) ]
-        left, answer = 0, 1
+        left, answer, total = 0, 0, 0
         
         for right in range(len(nums)):
-            for i in range(32):
-                count[i] += (nums[right] >> i) & 1
             
-            while not self.nice(count):
-                for i in range(32):
-                    count[i] -= (nums[left] >> i) & 1
+            while total & nums[right] != 0:
+                total ^= nums[left]
                 left += 1
-            
+                
+            total ^= nums[right]
             answer = max(answer, right - left + 1)
             
         return answer
-            
-    def nice(self, count):
-        for i in range(len(count)):
-            if count[i] > 1:
-                return False
-            
-        return True 
+     
             
         
             
