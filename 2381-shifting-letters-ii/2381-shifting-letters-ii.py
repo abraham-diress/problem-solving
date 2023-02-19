1b86f35ade41e7b5047a98975b5df36847aadee9
Abraham Diress <85117882+abraham-diress@users.noreply.github.com>
2023-02-20 00:23:17 +0200

    Time: 1413 ms (44.69%), Space: 39.3 MB (50.84%) - LeetHub

---
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def shift(self, c, shift):
+        return chr(((ord(c) - 97 + shift) % 26) + 97)
+
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        n = len(s)
+        arr = [0] * (n + 1) 
+        ans = ''
+
+        for start, end, dir in shifts:
+            if dir == 0:
+                arr[start] -= 1
+                arr[end + 1] += 1
+            else:
+                arr[start] += 1
+                arr[end + 1] -= 1
+        
+        for i in range(1, len(arr)):
+            arr[i] += arr[i - 1]
+
+        for i in range(len(s)):
+            ans += self.shift(s[i], arr[i])
+
+        return ans 
+
+
+        
+
+
+            
