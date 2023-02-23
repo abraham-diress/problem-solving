4db710a9008c9243fb50df2f82d52dd1852da186
Abraham Diress <85117882+abraham-diress@users.noreply.github.com>
2023-02-23 22:07:35 +0200

    Time: 2024 ms (39.64%), Space: 15.5 MB (9.51%) - LeetHub

---
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -0,0 +1,23 @@
+class Solution:
+    def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
+        new_list = list(zip(ages, scores))
+        new_list = sorted(new_list)
+        
+        @lru_cache(None)
+        def helper(i):
+            if i==0:
+                return new_list[i][1]
+            max_value = 0
+            for j in range(i):
+                if new_list[i][1]>=new_list[j][1]:
+                    max_value = max(max_value, helper(j))
+            
+            return max_value + new_list[i][1]
+        
+        return max(helper(i) for i in range(len(scores)))
+            
+            
+        
+        
+        
+        
