9efa79bd90a07d15f2e8abbbda6a670d4129b6ac
Abraham Diress <85117882+abraham-diress@users.noreply.github.com>
2023-02-25 17:45:03 +0200

    Time: 31 ms (73.57%), Space: 13.9 MB (11.11%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,45 +1,13 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if len(needle) > len(haystack):
-            return -1
-
-
-        length = len(needle)
-        c_sum = 0
-        mod = (10**9 +7)
-
-        for i in range(length-1, -1, -1):
-            c_sum += ((ord(needle[i]) - ord('a') + 1) * 10**(length - i -1 )) 
-
-        c_sum %= mod
-
-        cur = 0
-        l, r = 0, 0
-
-        while r < len(needle):
-            cur += ((ord(haystack[r]) - ord('a') + 1) * 10**(length - r - 1 ))
-            r += 1
+        ln = len(needle)
+        lh = len(haystack)
         
-        cur %= mod
+        for i in range(lh):
+            if (i + ln - 1) < lh and haystack[i: i + ln] == needle:
+                return i
 
-        if c_sum == cur:
-            return 0
-
-
-        while r < len(haystack):
-            ch_l = ord(haystack[l]) - ord('a') + 1
-            ch_r = ord(haystack[r]) - ord('a') + 1
-            cur = ((cur - (ch_l* 10**(length-1)))* 10) + ch_r
-            l += 1
-            r += 1
-
-            cur %= mod
-
-            if cur == c_sum:
-                return l
-        
-        return -1
-
+        return -1 
         
 
         
