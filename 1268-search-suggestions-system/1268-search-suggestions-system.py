193f0e9b228315d26c00fd0330c1f63b377a7c8e
Abraham Diress <85117882+abraham-diress@users.noreply.github.com>
2023-04-28 22:37:26 +0200

    Time: 698 ms (9.93%), Space: 24.8 MB (5.15%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -3,53 +3,52 @@ class Solution:
         self.root = TrieNode()
         
     def insert(self, word):
-        curr = self.root
+        cur = self.root
         
         for ch in word:
             idx = ord(ch) - ord('a')
-            if not curr.children[idx]:
-                curr.children[idx] = TrieNode()
-            curr = curr.children[idx]
-            
-        curr.isEnd = True
-        curr.word = word
-        
-    def dfs(self, node, lst):
-        if len(lst) == 3:
-            return lst
-        if node.isEnd:
-            lst.append(node.word)
-        
-        for child in node.children:
-            if child:
-                self.dfs(child, lst)
-    
-    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        for product in products:
-            self.insert(product)   
-        
-        curr = self.root
-        ans = []
-        
-        for ch in searchWord:
-            idx = ord(ch) - ord('a')
-            if curr and curr.children[idx]:
-                collections = []
-                curr = curr.children[idx]
-                self.dfs(curr, collections)
-                ans.append(collections)
+            if not cur.children[idx]:
+                cur.children[idx] = TrieNode()
                 
-            else:
+            cur = cur.children[idx]
+        
+        cur.end = word
+    
+    def dfs(self, cur_node, cur_list):
+        if len(cur_list) == 3:
+            return 
+        
+        if cur_node.end:
+            cur_list.append(cur_node.end)
+        
+        for ch in cur_node.children:
+            if ch:
+                self.dfs(ch, cur_list)
+            
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        ans = [] 
+        cur = self.root
+
+        for product in products:
+            self.insert(product)
+        
+         
+        for ch in searchWord:              
+            idx = ord(ch) - ord('a')
+            
+            if cur and cur.children[idx]:
+                temp = []
+                self.dfs(cur.children[idx], temp)
+                ans.append(temp)
+                cur = cur.children[idx]
+                
+            else:         
                 ans.append([])
-                curr = None
+                cur = None  
+        
         return ans 
-                    
         
 class TrieNode:
     def __init__(self):
         self.children = [None for _ in range(26)]
-        self.isEnd = False
-        self.word = ''
-        
-    
-        
+        self.end = None 
