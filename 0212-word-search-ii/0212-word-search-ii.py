ee86f3c722b33622135f76081314abc8a2024eff
Abraham Diress <85117882+abraham-diress@users.noreply.github.com>
2023-05-03 22:31:54 +0200

    Time: 9583 ms (5.07%), Space: 19.4 MB (5.30%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -0,0 +1,49 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = None
+
+    def addWord(self, word):
+        cur = self
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.isWord = word
+        
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+        for w in words:
+            root.addWord(w)
+        
+        ROWS, COLS = len(board), len(board[0])
+        res, visit = set(), set()
+        
+        def dfs(r, c, node):
+            if (r < 0 or c < 0 or
+               r == ROWS or c == COLS or
+               board[r][c] not in node.children or (r, c) in visit):
+                return 
+            visit.add((r, c))
+            node = node.children[board[r][c]]
+            if node.isWord:
+                res.add(node.isWord)
+            
+            dfs(r + 1, c, node)
+            dfs(r - 1, c, node)
+            dfs(r, c + 1, node)
+            dfs(r, c - 1, node)
+            
+            visit.remove((r, c))
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, root)
+            
+        return list(res)
+            
+            
+            
+        
+        
