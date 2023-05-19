727cdd356bc477fb9b0f93f5b47929fb7832d791
Abraham Diress <85117882+abraham-diress@users.noreply.github.com>
2023-05-19 14:48:54 +0200

    Time: 139 ms (27.21%), Space: 18 MB (31.42%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,26 +1,27 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        res = 0
-        cnt = 0
+        ans = 0 
+        cnt = 0 
+        
         for i in range(32):
-            curSum = 0
+            curSum = 0 
             for num in nums:
                 if num < 0:
-                    num = -1 * (num) 
+                    num = -1 * num
                 if num & (1 << i):
                     curSum += 1
+                    
             if curSum % 3:
-                res |= (1 << i)
+                ans |= (1 << i)
         
         for num in nums:
-            if res == num:
+            if num == ans:
                 cnt += 1
         
         if cnt == 1:
-            return res
+            return ans 
         else:
-            return -1*res 
-
+            return ans * -1
 
 
 
