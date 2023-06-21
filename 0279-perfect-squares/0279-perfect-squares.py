ad62d4d77e523751d2ca54bd15ec3cc185b5ff97
Abraham Diress <85117882+abraham-diress@users.noreply.github.com>
2023-06-21 23:07:58 +0300

    Time: 3847 ms (63.52%), Space: 16.5 MB (56.39%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -4,9 +4,14 @@ class Solution:
         dp[0] = 0
         dp[1] = 1
         
+        squares = [0]
+        
+        for j in range(1, math.isqrt(n) + 1):
+            squares.append(j**2)
+        
         for i in range(2, n + 1):
             for j in range(1, math.isqrt(i) + 1):
-                 dp[i] = min(dp[i], 1 + dp[i - (j**2)])
+                 dp[i] = min(dp[i], 1 + dp[i - squares[j]])
                     
         return dp[n]
         
