001085ec7952f8f28b822b12d9bb8557b7ffd874
Abraham Diress <85117882+abraham-diress@users.noreply.github.com>
2023-06-22 21:26:50 +0300

    Time: 124 ms (5.75%), Space: 17.2 MB (5.01%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,16 +1,23 @@
 class Solution:
+    def longestCommonSubsequence(self, s1: str, s2: str) -> int:
+        n, m = len(s1), len(s2)
+
+        dp = [[-1] * (m+1) for _ in range(n+1)]
+
+        for i in range(n+1):
+            dp[i][0] = 0
+            
+        for i in range(m+1):
+            dp[0][i] = 0
+            
+        for ind1 in range(1, n+1):
+            for ind2 in range(1, m+1):
+                if s1[ind1-1] == s2[ind2-1]:
+                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1]
+                else:
+                    dp[ind1][ind2] = max(dp[ind1-1][ind2], dp[ind1][ind2-1])
+                    
+        return dp[n][m]
+
     def isSubsequence(self, s: str, t: str) -> bool:
-        
-        i, j = 0, 0
-        
-        while i < len(s) and j < len(t):
-            if s[i] == t[j]:
-                i += 1
-                j += 1
-            else:
-                j += 1     
-        return True if i == len(s) else False            
-        
-        
-        
-        
+        return self.longestCommonSubsequence(s, t) == len(s)
