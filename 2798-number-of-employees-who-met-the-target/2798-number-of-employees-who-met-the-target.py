e26da945502274964d74b37ccfa33ca176d00d59
Abraham Diress <85117882+abraham-diress@users.noreply.github.com>
2024-01-18 01:00:43 +0200

    Time: 23 ms (100.00%), Space: 17.3 MB (28.73%) - LeetHub

--- 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.py
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.py
@@ -1,7 +1,6 @@
 class Solution:
     def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
         cnt = 0
-        
         for hour in hours:
             if hour >= target:
                 cnt += 1
