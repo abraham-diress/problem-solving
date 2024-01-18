8b237e466cc020c637d0ab1c71ac0abba643f0d9
Abraham Diress <85117882+abraham-diress@users.noreply.github.com>
2024-01-19 01:00:23 +0200

    Time: 288 ms (99.05%), Space: 17.3 MB (18.75%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.py
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.py
@@ -1,6 +1,6 @@
 class Solution:
     def findNonMinOrMax(self, nums: List[int]) -> int: 
-        return heapq.nlargest(2, nums)[-1] if len(nums) > 2 else -1
         
+        return sorted(nums[:3])[1] if len(nums) > 2 else -1
             
-        
+                                                                                              
