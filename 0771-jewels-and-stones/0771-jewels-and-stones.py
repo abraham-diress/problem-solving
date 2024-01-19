8a9b97239406309799b215f58105e881b7b4c763
Abraham Diress <85117882+abraham-diress@users.noreply.github.com>
2024-01-20 00:51:42 +0200

    Time: 38 ms (57.70%), Space: 16.5 MB (53.52%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        jewels = Counter(jewels)
+        res = 0
+        
+        for stone in stones:
+            if stone in jewels:
+                res += 1
+        
+        return res
+        
+        
