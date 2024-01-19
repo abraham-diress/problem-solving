7d0fe2fe52c12d71fb754d8f5de210c771820dc0
Abraham Diress <85117882+abraham-diress@users.noreply.github.com>
2024-01-20 00:58:08 +0200

    Time: 31 ms (90.40%), Space: 16.4 MB (53.52%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,12 +1,10 @@
 class Solution:
-    def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        jewels = Counter(jewels)
+    def numJewelsInStones(self, jewels: str, stones: str) -> bool:
         res = 0
         
-        for stone in stones:
-            if stone in jewels:
-                res += 1
+        for jewel in jewels:
+            for stone in stones:
+                if jewel == stone:
+                    res += 1
         
         return res
-        
-        
