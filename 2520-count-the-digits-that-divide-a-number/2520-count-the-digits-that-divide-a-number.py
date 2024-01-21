fa49fb9057122eeb6f5943d65c79b3e556070337
Abraham Diress <85117882+abraham-diress@users.noreply.github.com>
2024-01-22 01:50:08 +0200

    Time: 40 ms (36.57%), Space: 16.5 MB (52.96%) - LeetHub

--- 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.py
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.py
@@ -1,13 +1,13 @@
 class Solution:
     def countDigits(self, num: int) -> int:
-        num_copy = num
-        cnt = 0
+        
+        def getValidDigit(num):
+            num_copy = num
+            while num_copy > 0:
+                digit = num_copy % 10  
+                num_copy = num_copy // 10  
 
-        while num_copy > 0:
-            digit = num_copy % 10  
-            num_copy = num_copy // 10  
-
-            if digit != 0 and num % digit == 0:
-                cnt += 1
-
-        return cnt
+                if digit != 0 and num % digit == 0:
+                    yield digit
+        
+        return sum(1 for _ in getValidDigit(num))
