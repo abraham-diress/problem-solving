20accb3130a6bc2bd392d19e44616af1061b9386
Abraham Diress <85117882+abraham-diress@users.noreply.github.com>
2024-01-23 02:11:02 +0200

    Time: 30 ms (91.91%), Space: 16.5 MB (55.17%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.py
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -1,5 +1,11 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        return s.lower()
+        res = ""
+        for char in s:
+            if 'A' <= char <= 'Z':  
+                res += chr(ord(char) + ord('a') - ord('A'))  
+            else:
+                res += char
         
+        return res
         
