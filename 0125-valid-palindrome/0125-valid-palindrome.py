39c8406029b21ac871c8cfae69d735360c3167b9
Abraham Diress <85117882+abraham-diress@users.noreply.github.com>
2025-02-06 20:34:37 +0200

    Time: 11 ms (36.63%), Space: 18.1 MB (53.02%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,10 +1,20 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        stack = [c.lower() for c in s if c.isalnum()]
-        
-        for c in s:
-            if c.isalnum():
-                if not stack or c.lower() != stack.pop():
-                    return False
-        
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+                continue
+
+            if not s[r].isalnum():
+                r -= 1
+                continue
+
+            if s[l].lower() != s[r].lower():
+                return False
+
+            l += 1
+            r -= 1
+
         return True
